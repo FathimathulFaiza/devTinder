6e7658fefd1e67fe060174311a96d5c4b21f9394
Fathimathul Faiza <paachi786@Fathimathul-Faizas-MacBook-Air.local>
2025-11-23 18:06:05 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -3,14 +3,23 @@ const express = require('express')
 
 const app = express()
 
+
+app.get('/user',(req,res)=>{
+    res.send({firstName : "Fathimathul", secondName : "Faiza"})
+})
+
+app.post('/user',(req,res)=>{
+    res.send("data posted in the database successfully")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("user deleted successfully")
+})
+
 app.use("/",(req,res)=>{
     res.send("WELCOME FROM HOME PAGE")
 })
 
-
-app.use('/user',(req,res)=>{
-    res.send("Helllo from the server...")
-})
 
 
 app.listen(3000,()=> console.log("server running on port 3000")) 
