6c71b8f5b99dc7823fe33580638b799a3b5a7c52
Fathimathul Faiza <paachi786@Fathimathul-Faizas-MacBook-Air.local>
2025-11-24 12:27:11 +0530

    learning middlewares

--- src/app.js
+++ src/app.js
@@ -4,63 +4,20 @@ const express = require('express')
 const app = express()
 
 
-// req.query
 
-app.get('/user',(req,res)=>{         // http://localhost:3000/user?userId=2000    =>  { 'userId ': ' 2000' }
+app.use("/",(req,res,next)=>{
+     
+      res.send("WELCOME FROM HOME PAGE")
+      
+         next()
 
-console.log(req.query)
-
-    res.send({firstName : "Fathimathul", secondName : "Faiza"})
 })
 
-
-// req.params
-
-app.get('/users/:userName',(req,res)=>{
-    console.log(req.params)
-
-    res.send({newUser : "Asif",password : "aachi786"})
-})
-
-
-// ex : req.query
-
-app.get('/product',(req,res)=>{      //http://localhost:3000/product?productName=mango  =>  the name of the product is mango
-
-    console.log(req.query)
-    
-    const productName = req.query.product
-
-    res.send(`the name of the product is ${req.query.productName}`)
+app.use('/',(req,res)=>{
+    res.send("this is the 2nd '/' route by calling the next() ")
 })
 
 
 
-// ex : req.params
 
-app.get('/product/:price',(req,res)=>{     // http://localhost:3000/product/800   => 
-   
-    console.log(req.params)
-
-    const productPrice = req.params.price
-
-    res.send(`price of the product is here ${productPrice}`)
-})
-
-
-
-app.post('/user',(req,res)=>{
-    res.send("data posted in the database successfully")
-})
-
-app.delete('/user',(req,res)=>{
-    res.send("user deleted successfully")
-})
-
-// app.use("/",(req,res)=>{
-//     res.send("WELCOME FROM HOME PAGE")
-// })
-
-
-
-app.listen(3000,()=> console.log("server running on port 3000")) 
+app.listen(7777,()=> console.log("server running on port 7777")) 
