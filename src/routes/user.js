abb79703cadde62817140888d392deccd4d5c050
Fathimathul Faiza <fathimthulfaiza@gmail.com>
2025-12-06 00:03:15 +0530

    $or Query, Created view/connection Api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const { userAuth } = require('../middlewares/auth')
 const userRouter = express.Router()
-const connectionRequest = require('../models/connectionRequest')
+const ConnectionRequest = require('../models/connectionRequest')
 
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender skills " // data shown for the user  
 
 // api for getting all the pending request for the logged in user
 
@@ -12,10 +13,10 @@ userRouter.get('/user/requests/recieved', userAuth, async (req,res)=>{
         const loggedInUser = req.user
 
 // find the connection requests
-        const connectionRequests = await connectionRequest.find({
+        const connectionRequests = await ConnectionRequest.find({
             toUserId : loggedInUser._id,
             status : "interested"
-        }).populate("fromUserId", ["firstName", "lastName", "photoUrl", "age", "gender", "skills", "about"]) // -> asking to user collection to give the details of firstName & lastName along with (JOINING 2 COLLECTIONS)
+        }).populate("fromUserId",USER_SAFE_DATA) // -> asking to user collection to give the details of firstName & lastName along with (JOINING 2 COLLECTIONS)
 
 
         res.json({
@@ -30,6 +31,32 @@ userRouter.get('/user/requests/recieved', userAuth, async (req,res)=>{
 
      } 
     
+})
+
+
+
+// retrieve all connections of the log
+
+userRouter.get('/user/connections', userAuth, async (req,res)=>{
+    try{
+
+        const loggedInUser = req.user
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or :[
+                {toUserId : loggedInUser._id, status : "accepted"}, //requests the user accepted from others, and requests the user sent — as long as status is accepted.
+                {fromUserId : loggedInUser._id, status : "accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA)
+
+        const data = connectionRequests.map((row) => row.fromUserId)
+
+        res.json({data})
+
+    }
+    catch(err){
+        res.status(400).send({message : err.message})
+    }
 })
 
 
